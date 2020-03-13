01cd9fe252151ac9541dd9341987ee9f37510b80
Jenner <5179202+jenner-luo@users.noreply.github.com>
2020-03-13 17:55:53 +0800

    Update docker-operations.sh

--- docker-operations.sh
+++ docker-operations.sh
@@ -2,11 +2,10 @@
 #Create By lzn
 
 docker-image-grep(){
-   local grep_str=${1:=""}
-  docker images|grep $grep_str|awk '{print $1":"$2}'
+   local grep_str=${1:-""}
+   docker images|grep $grep_str|awk '{print $1":"$2}'
 }
-
 docker-rmi-grep(){
-   local grep_str=${1:=""}
-  docker rmi $(docker images|grep $grep_str|awk '{print $1":"$2}')
+   local grep_str=${1:-""}
+   docker rmi $(docker images|grep $grep_str|awk '{print $1":"$2}')
 }
