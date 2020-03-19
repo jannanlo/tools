0eaf2e15f11dfad1e823446880487c364063ab80
Jenner <5179202+jenner-luo@users.noreply.github.com>
2020-03-19 14:56:22 +0800

    Update mysql-opertaion.sh

--- mysql-opertaion.sh
+++ mysql-opertaion.sh
@@ -24,7 +24,7 @@ check-db-talbes-size(){
 SELECT
 table_schema as "数据库",
 table_name as "表名",
-table_rows as "记录数",
+table_rows as "行数",
 truncate(data_length/1024/1024, 2) as "数据容量(MB)",
 truncate(index_length/1024/1024, 2) as "索引容量(MB)"
 FROM information_schema.tables  
@@ -32,7 +32,21 @@ WHERE table_schema="'${db}'"
 order by data_length desc, index_length desc;
 '''
 }
-
+check-dbs-size(){
+  local user=${1:-"userName"}
+  local paswd=${2:-"password"}
+  mysql -u$user -p$paswd -e '''
+SELECT
+table_schema as "数据库",
+table_name as "表名",
+sum(table_rows) as "行数",
+sum(truncate(data_length/1024/1024, 2)) as "数据容量(MB)",
+sum(truncate(index_length/1024/1024, 2)) as "索引容量(MB)"
+FROM information_schema.tables  
+GROUP BY table_schema
+ORDER BY sum(data_length) desc, sum(index_length) desc;
+'''
+}
 main(){
   $*
 }
