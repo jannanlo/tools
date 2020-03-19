c9b739b91e3dac72c25738923ad3315a84c6b377
Jenner <5179202+jenner-luo@users.noreply.github.com>
2020-03-19 14:13:01 +0800

    add check-db-talbes-size

--- mysql-opertaion.sh
+++ mysql-opertaion.sh
@@ -16,7 +16,24 @@ AND table_name="'${tb}'";
 '''
 }
 
+check-db-talbes-size(){
+  local db=${1:-"dbName"}
+  local user=${2:-"userName"}
+  local paswd=${3:-"password"}
+  mysql -u$user -p$paswd -e '''
+SELECT
+table_schema as "数据库",
+table_name as "表名",
+table_rows as "记录数,
+truncate(data_length/1024/1024, 2) as "数据容量(MB)",
+truncate(index_length/1024/1024, 2) as "索引容量(MB)
+FROM information_schema.tables  
+WHERE table_schema="'${db}'"
+order by data_length desc, index_length desc;
+'''
+}
+
 main(){
-  check-db-talbe-size
+  check-db-talbes-size
 }
 main
