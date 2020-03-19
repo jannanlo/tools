64fe7e533731c9f10667f8b02f0ef413916c45bd
Jenner <5179202+jenner-luo@users.noreply.github.com>
2020-03-19 14:45:24 +0800

    Update mysql-opertaion.sh

--- mysql-opertaion.sh
+++ mysql-opertaion.sh
@@ -24,9 +24,9 @@ check-db-talbes-size(){
 SELECT
 table_schema as "数据库",
 table_name as "表名",
-table_rows as "记录数,
+table_rows as "记录数",
 truncate(data_length/1024/1024, 2) as "数据容量(MB)",
-truncate(index_length/1024/1024, 2) as "索引容量(MB)
+truncate(index_length/1024/1024, 2) as "索引容量(MB)"
 FROM information_schema.tables  
 WHERE table_schema="'${db}'"
 order by data_length desc, index_length desc;
@@ -34,6 +34,6 @@ order by data_length desc, index_length desc;
 }
 
 main(){
-  check-db-talbes-size
+  $*
 }
-main
+main "$*"
